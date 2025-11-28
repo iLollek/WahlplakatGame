7b73314414c8033734b5b69f7dd41d85365c76f8
iLollek <loris_06@yahoo.de>
2025-11-28 20:53:43 +0100

    Added prod

--- src/Server/main.py
+++ src/Server/main.py
@@ -1,9 +1,28 @@
 from DatabaseService import DatabaseService
 from NetworkService import NetworkService
-import GameServer  # ← Modul importieren, nicht die Variable
+import GameServer
 import threading
+import sys
+import os
+import logging
 
-env = DatabaseService.get_sillyorm_environment(use_postgres=False)
+if getattr(sys, 'frozen', False):
+    print(f'Running in Productive (PROD) Environment (.exe)')
+    program_directory = os.path.dirname(os.path.abspath(sys.executable))
+    ENV = "PROD"
+    logging.basicConfig(filename=f'server.log', filemode='a', format='%(name)s - %(levelname)s - %(funcName)20s() - %(message)s', level=logging.DEBUG, force=True)
+else:
+    print(f'Running in Development (DEV) Environment (.py)')
+    program_directory = os.path.dirname(os.path.abspath(__file__))
+    ENV = "DEV"
+    logging.basicConfig(stream=sys.stdout, format='%(name)s - %(levelname)s - %(funcName)20s() - %(message)s', level=logging.DEBUG, force=True)
+os.chdir(program_directory)
+
+if ENV == "PROD":
+    DatabaseService.PATH_TO_YOUR_CONNECTION_STRING_FILE = r"connection_string.txt"
+    env = DatabaseService.get_sillyorm_environment(use_postgres=False)
+else:
+    ENV == "DEV"
 
 # Initialisiere GameService
 GameServer.init_game_service(env)
